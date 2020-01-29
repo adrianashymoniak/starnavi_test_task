4f005c5e611bfeeafb784852c9e9e46604473772
Adriana Shymoniak <adrianashymoniak@gmail.com>
2020-01-29 20:54:43 +0200

    Added custom permissions and api_root method

--- social_network/core/urls.py
+++ social_network/core/urls.py
@@ -4,8 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from core import views
 
 urlpatterns = [
-    path('posts/', views.PostsList.as_view()),
+    path('posts/', views.PostsList.as_view(), name='posts-list'),
     path('posts/<int:pk>/', views.PostsDetail.as_view()),
+    path('', views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- social_network/core/views.py
+++ social_network/core/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from core.models import Post
 from core.serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'posts': reverse('posts-list', request=request, format=format)
+    })
 
 
 class PostsList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -18,4 +30,5 @@ class PostsList(generics.ListCreateAPIView):
 class PostsDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
